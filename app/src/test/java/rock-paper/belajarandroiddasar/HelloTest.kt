8f9157571d11630ff83bd7db23afdb328dd13918
nurkh <nurkholisalekanjarmaruf88@gmail.com>
2024-01-15 11:08:54 +0700

    update

---
+++ app/src/test/java/rock-paper/belajarandroiddasar/HelloTest.kt
@@ -0,0 +1,14 @@
+package rock-paper
+
+import org.junit.Assert
+import org.junit.Test
+
+class HelloTest {
+
+    @Test
+    fun testSayHello() {
+        val result = Hello.sayHello("Eko")
+
+        Assert.assertEquals("Hello Eko", result)
+    }
+}
